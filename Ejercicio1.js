d9667121fe2285bfc9ab10dc37ce3a2026a7911c
sharymhub <sharaynicollew@gmail.com>
2024-08-18 21:02:51 -0500

    Actividades de la 1° semana

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+let contarnumeros= 0;
+let restantes 
+
+for (let numero = 1; numero <= 10; numero ++){
+    console.log(numero);
+    if (numero > 5) {
+        contarnumeros ++;
+    }
+}
+console.log ("Numeros mayores a 5 = " + contarnumeros);
---
+++ Ejercicio2.js
@@ -0,0 +1,14 @@
+const colores = ["Azul", "Rojo","Verde", "Negro"];
+console.log (colores [0], colores[3]);
+
+//agregar otro elemento//
+colores.push ("Rosado");
+console.log (colores);
+
+//Eliminar el primer color//
+
+colores.shift();
+console.log (colores);
+
+//Cuantos elementos hay en el array//
+console.log (colores.length);
